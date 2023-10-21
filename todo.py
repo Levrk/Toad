96a18e25ee01434e34730eda16866ee7d4c8381c
Levrk <levrolandkalb@gmail.com>
2023-10-21 02:05:21 -0400

    implemented promote/demote/add/delete

--- todo.py
+++ todo.py
@@ -9,7 +9,9 @@ def todo():
         return
     if (userInput[0] == '+'):
         #adding element to todo list
+        print(userInput[1:])
         add(userInput[1:])
+        read()
     else:
         try:
             n = int(userInput[0:-1])
@@ -20,17 +22,21 @@ def todo():
         if (n > count or n < 1):
             #index out of bounds
             print("your list has no element ", n)
-        elif (command == '+'):
+        elif (command == '^'):
             #promoting element
             promote(n)
+            read()
         elif (command == '-'):
             #demoting element
             demote(n)
+            read()
         elif (command == '!'):
             #deleting element
             delete(n)
+            read()
 
 def read():
+    print("To Do: \n")
     with open('files/todo.csv', 'r') as csv_file:
         reader = csv.reader(csv_file)
         count = 1
@@ -42,19 +48,63 @@ def read():
         
 
 def promote(item):
-    print("promote ", item)
+    # Read the CSV file
+    with open("files/todo.csv", 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+
+    # Move the nth line to the top
+    line_to_move = data.pop(item - 1)
+    data.insert(0, line_to_move)
+
+    # Write the modified data to a new CSV file
+    with open("files/todo.csv", 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
     return
 
 def demote(item):
-    print("demote ", item)
+    # Read the CSV file
+    with open("files/todo.csv", 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+
+    # Move the nth line to the top
+    line_to_move = data.pop(item - 1)
+    data.append(line_to_move)
+
+    # Write the modified data to a new CSV file
+    with open("files/todo.csv", 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
     return
 
 def delete(item):
-    print("delete ", item)
+    # Read the CSV file
+    with open("files/todo.csv", 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+
+    # delete the nth line to the top
+    data.pop(item - 1)
+
+    # Write the modified data to a new CSV file
+    with open("files/todo.csv", 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
     return
 
 def add(item):
-    print("add ", item)
+        # Read the CSV file
+    with open("files/todo.csv", 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+
+    # delete the nth line to the top
+    data.insert(0, [item])
+
+    # Write the modified data to a new CSV file
+    with open("files/todo.csv", 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
     return
-
-
