c1be487e720a2936a6e8e5866cbea029319ffdfb
Levrk <levrolandkalb@gmail.com>
2023-10-22 08:43:43 -0400

    added endless timer

--- focus.py
+++ focus.py
@@ -5,38 +5,34 @@ import time
 import sys
 from playsound import playsound
 
-
-# Create a condition variable for synchronization with timer stopped
-condition = threading.Condition()
-
 def main(input):
     # Global variable to track mouse movement
     startTime = time.time()
-    endTime = int(input)
+    try:
+        endTime = int(input)
+    except:
+        endTime = 0
     # Function to be called when the mouse is moved
     def on_move(x, y):
         global mouse_moved
         mouse_moved = True
         
-
     def timerComplete():
         #play good sound on timer completion
         playsound("files/good.mp3")
         sys.exit()
-    
-    
+
     def timerStopped():
         #timerStopped is triggered when the script does not register any mouse movement for 5 consecutive minutes
-        playsound("files/bad.mp3")
-        
+        playsound("files/bad.mp3")       
 
     def timer():
         #to be executed repeatedly while focus.py script is active
         global mouse_moved
         mouse_moved = False
-        # Wait for 10 seconds (you can adjust the duration)
-        for _ in range(10):
-            if (time.time() >= (startTime + endTime)):
+        # Wait for 5 minutes (you can adjust the duration)
+        for _ in range(5): ###demo version
+            if (time.time() >= (startTime + endTime) and endTime != 0):
                 timerComplete()
             if mouse_moved: 
                 return
